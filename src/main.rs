1859d837c1dc0274d6026f763efb0fd26a04d446
addrian-77 <lunguadrian30@gmail.com>
2025-03-19 11:15:14 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use core::panic::PanicInfo;
+use cortex_m_semihosting::hprintln;
+
+#[entry]
+fn main() -> ! {
+    // Your bare-metal logic here (GPIO setup, etc.)
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    // Print a panic message
+    hprintln!("Panic occurred: {:?}", info);
+
+    // Enter an infinite loop to halt the system after a panic
+    loop {}
+}
